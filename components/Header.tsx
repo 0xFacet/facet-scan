78e3b4424bf1b47237216ff905f669cd082cb70b
Michael Hirsch <michael@hirsch.me>
2023-11-20 21:46:52 -0600

    add mobile navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,9 @@ import { NavLink } from "./NavLink";
 import { Section } from "./Section";
 import { SectionContainer } from "./SectionContainer";
 import Image from "next/image";
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Button } from "./ui/button";
+import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 
 export const Header = () => {
   return (
@@ -16,26 +19,54 @@ export const Header = () => {
             <Image
               src="/facet-scan-lockup.svg"
               alt="Facet Scan"
-              width={167.7}
-              height={32}
+              width={150}
+              height={30}
               priority
-              className="hidden sm:block"
-            />
-            <Image
-              src="/facet-brandmark.svg"
-              alt="Facet Scan"
-              width={32}
-              height={32}
-              priority
-              className="block sm:hidden"
             />
           </Link>
-          <div className="flex gap-8 items-center h-min-full">
-            <div className="flex gap-4 h-min-full">
+          <div className="flex gap-4 sm:gap-8 items-center h-16">
+            <div className="gap-4 h-min-full hidden lg:flex">
+              <NavLink href="/blocks">Blocks</NavLink>
               <NavLink href="/txs">Transactions</NavLink>
               <NavLink href="/contracts">Contracts</NavLink>
             </div>
             <CustomConnectButton />
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button className="lg:hidden" size="icon" variant="outline">
+                  <HamburgerMenuIcon className="h-6 w-6" />
+                  <span className="sr-only">Toggle navigation menu</span>
+                </Button>
+              </SheetTrigger>
+              <SheetContent side="top">
+                <div className="flex flex-1 flex-col gap-4 overflow-auto pt-16 px-4 pb-4 sm:pt-20 sm:px-8 sm:pb-8">
+                  <SheetClose asChild>
+                    <Link
+                      className="group grid h-auto w-full items-center justify-start gap-1 rounded-md p-4 text-sm font-medium transition-colors border border-line hover:border-primary"
+                      href="/blocks"
+                    >
+                      Blocks
+                    </Link>
+                  </SheetClose>
+                  <SheetClose asChild>
+                    <Link
+                      className="group grid h-auto w-full items-center justify-start gap-1 rounded-md p-4 text-sm font-medium transition-colors border border-line hover:border-primary"
+                      href="/txs"
+                    >
+                      Transactions
+                    </Link>
+                  </SheetClose>
+                  <SheetClose asChild>
+                    <Link
+                      className="group grid h-auto w-full items-center justify-start gap-1 rounded-md p-4 text-sm font-medium transition-colors border border-line hover:border-primary"
+                      href="/contracts"
+                    >
+                      Contracts
+                    </Link>
+                  </SheetClose>
+                </div>
+              </SheetContent>
+            </Sheet>
           </div>
         </div>
       </Section>
